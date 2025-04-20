72d3e4e96e779eb4c4773f79d73a862118662445
rangersking <maxlevy80@yahoo.com>
2025-04-19 21:27:10 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -39,7 +39,7 @@ if ingredients_list:
         smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/" + search_on)
         sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
 
-    #st.write(ingredients_string)
+    st.write(ingredients_string)
 
     my_insert_stmt = """ insert into smoothies.public.orders
             values (default,default,'""" + name_on_order  + """', ' """+ingredients_string+"""', default)"""
