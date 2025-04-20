a8469016e36973d2544c257edc827313e61ab4d8
rangersking <maxlevy80@yahoo.com>
2025-04-19 20:31:09 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -42,7 +42,7 @@ if ingredients_list:
     #st.write(ingredients_string)
 
     my_insert_stmt = """ insert into smoothies.public.orders
-            values ('""" + ingredients_string + """', ' """+name_on_order+"""', default, default)"""
+            values (default,default,'""" + name_on_order  + """', ' """+ingredients_string+"""', default)"""
 
     #st.write(my_insert_stmt)
     time_to_insert = st.button('Submit Order')
